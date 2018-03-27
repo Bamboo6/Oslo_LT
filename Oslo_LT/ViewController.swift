57814c1ce7e2b95ef2833cd7223550be215b6327
Bamboo6 <dream1718@foxmail.com>
2018-03-27 11:25:50 +0800

    Initial Commit

---
+++ Oslo_LT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oslo_LT
+//
+//  Created by LT on 2018/3/27.
+//  Copyright © 2018年 LT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
